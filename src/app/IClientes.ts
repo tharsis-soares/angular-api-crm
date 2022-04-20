a99c056f933e381a396f4545deadc05218203ead
unknown <tharsissoares@hotmail.com>
2022-04-20 16:49:16 -0300

    'teste'

---
+++ src/app/IClientes.ts
@@ -0,0 +1,13 @@
+export interface Clientes {
+    nm_cliente: string
+    nu_documento: string
+    ds_cep: any
+    ds_endereco: any
+    ds_bairro: any
+    ds_complemento: any
+    nu_cliente: number
+    nm_estado: any
+    nm_cidade: any
+    qtd_vendas: number
+    ds_email: string
+}
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { API_PATH} from '../environments/environment';
-import { Vendas } from './IMeiosPagamento';
 import { Observable } from 'rxjs';
+import { Clientes } from './IClientes';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +11,9 @@ export class ApiService {
    
   constructor(private httpClient: HttpClient) { }
 
-  getVendas(): Observable<Vendas[]> {
+  getVendas(): Observable<Clientes[]> {
     const myHeaders = new HttpHeaders({'Content-Type': 'application/json', 'Client-Code': 'FC-SB-15', 'Client-key': '6ea297bc5e294666f6738e1d48fa63d2'});
-    return this.httpClient.get<Vendas[]>(`${API_PATH}clientes`, {headers: myHeaders});
+    return this.httpClient.get<Clientes[]>(`https://api-sandbox.fpay.me/clientes`, {headers: myHeaders});
   }
 
 }
